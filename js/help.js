8a385a8e6c64ead9083003a8c74a7efd1178b71b
Daniel <debem1972@gmail.com>
2025-06-29 19:13:24 -0300

    Foram deletados os arquivos antigos e inúteis ao projeto.

--- js/help.js
+++
@@ -1,7 +0,0 @@
-//Chama o audio de ajuda quando clicar no botão com icone de 'i' ao lado do título na página de dados 
-
-let ajuda = document.querySelector('.help');
-let audioAjuda = document.querySelector('#helpi');
-ajuda.addEventListener('click', function () {
-    audioAjuda.play();
-});
